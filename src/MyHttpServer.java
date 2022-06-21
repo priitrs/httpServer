fa27199f6aeb5906ff952e79f8ccbda7c99383de
priitrs <priitrs@gmail.com>
2022-06-21 12:26:01 +0300

    Server working

--- src/MyHttpServer.java
+++ src/MyHttpServer.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -6,6 +8,7 @@ import java.util.Date;
 public class MyHttpServer implements Runnable {
 
     public Socket socket;
+
 
     public MyHttpServer(Socket c) {
         this.socket = c;
@@ -34,85 +37,53 @@ public class MyHttpServer implements Runnable {
     @Override
     public void run() {
         System.out.println("Thread run() started");
+        while (true) {
 
-        try {
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            String inputLine = in.readLine();
-            String[] request = inputLine.split(" ");
-            System.out.println(inputLine);
-            inputLine = in.readLine();
-            System.out.println(inputLine);
-            inputLine = in.readLine();
-            System.out.println(inputLine);
-            inputLine = in.readLine();
-            System.out.println(inputLine);
-            inputLine = in.readLine();
-            System.out.println(inputLine);
-            inputLine = in.readLine();
-            System.out.println(inputLine);
-            inputLine = in.readLine();
-            System.out.println(inputLine);
-            inputLine = in.readLine();
-            System.out.println(inputLine);
-            inputLine = in.readLine();
-            System.out.println(inputLine);
-            inputLine = in.readLine();
-            System.out.println(inputLine);
-            inputLine = in.readLine();
-            System.out.println(inputLine);
-            inputLine = in.readLine();
-            System.out.println(inputLine);
-            inputLine = in.readLine();
-            System.out.println(inputLine);
-            inputLine = in.readLine();
-            System.out.println(inputLine);
-            inputLine = in.readLine();
-            System.out.println(inputLine);
-            inputLine = in.readLine();
-            System.out.println(inputLine+"end");
+            try {
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String inputLine = in.readLine();
+                System.out.println(inputLine);
 
-            String pathname = "";
-            if (request[1].equals("/")) {
-                pathname = "index.html";
-            } else {
-                pathname = request[1].replaceFirst("/","");
+                String[] request = inputLine.split(" ");
+                String pathname = "";
+                if (request[1].equals("/")) {
+                    pathname = "index.html";
+                } else {
+                    pathname = request[1].replaceFirst("/", "");
+                }
+
+                File file = new File(pathname);
+                byte[] fileData = readFileData(file);
+
+                PrintWriter out = new PrintWriter(socket.getOutputStream());
+                out.println(request[2] + " 200 OK");
+                out.println("PRIIT Server 1.0!");
+                out.println("Date: " + new Date());
+//                out.println("Content-type: " + "text/html");
+                out.println("Content-length: " + (int) file.length());
+                out.println("");
+                out.flush();
+
+                BufferedOutputStream dataOut = new BufferedOutputStream(socket.getOutputStream());
+                dataOut.write(fileData, 0, (int) file.length());
+                dataOut.flush();
+
+            } catch (IOException e) {
+                System.out.println("This error occured:\n");
+                e.printStackTrace();
             }
-            System.out.println(pathname);
-
-            File file = new File(pathname);
-            byte[] fileData = readFileData(file);
-
-            PrintWriter out = new PrintWriter(socket.getOutputStream());
-            out.println("HTTP/1.1 200 OK");
-            out.println("PRIIT Server 1.0!");
-            out.println("Date: " + new Date());
-            out.println("Content-type: " + "text/html");
-            out.println("Content-length: " + (int)file.length());
-            out.println("");
-            out.flush();
-
-            BufferedOutputStream dataOut = new BufferedOutputStream(socket.getOutputStream());
-            dataOut.write(fileData,0,(int)file.length());
-            dataOut.flush();
-
-
-
-        } catch (IOException e) {
-            System.out.println("This error occured:\n");
-            e.printStackTrace();
         }
 
     }
 
     private byte[] readFileData(File file) throws IOException {
         FileInputStream fileInputStream = null;
-        byte[] filedata = new byte[(int)file.length()];
+        byte[] filedata = new byte[(int) file.length()];
         try {
             fileInputStream = new FileInputStream(file);
             fileInputStream.read(filedata);
 
-        }finally
-        {
+        } finally {
             if (fileInputStream != null)
                 fileInputStream.close();
         }
